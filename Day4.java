e5fb0c4f974d6b215b7efd947bd72d2d7e66b88a
nwanaa <winituuu@gmail.com>
2024-09-05 19:52:28 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,7 @@
+public class Day4 {
+
+    public static void main(String[] args) {
+        System.out.print("3726 mdpl");
+        System.out.printf("\nnama %s, tinggi badan %d cm", "wana", 153);
+    }
+}
