612f842b038846e19bbfa32f772e838a218ef2b3
nwanaa <winituuu@gmail.com>
2024-09-06 05:22:00 -0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,15 @@
+package Day5;
+public class Day5 {
+    public static void main(String[] args) {
+        // method main adalah titik awal eksekusi dari sebuah program java
+        System.out.println("Nama: Nirwana");
+        // println: untuk mencetak teks ke baris berikutnya
+        System.out.print("3726 mdpl");
+        // print: untuk mencetak teks dengan baris yang sama atau mencetak teks apa adanya tanpa berpindah ke baris baru
+        System.out.printf("\nnama %s, tinggi badan %d cm", "wana", 153 );
+        /* printf: untuk mencetak teks dengan format tertentu. 
+        %s : format untuk mencetak type data string
+        %d : format untuk bilangan bulat desimal
+        /n : untuk membuat baris baru*/
+    }
+}
