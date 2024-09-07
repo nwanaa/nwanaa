a02ecc25ef00da2bad99e9921868b613d87d76dc
nwanaa <winituuu@gmail.com>
2024-09-07 17:48:14 +0800

    Update Day6.java

--- Day6.java
+++ Day6.java
@@ -1,5 +1,7 @@
 public class Day6 {
     public static void main(String[] args) {
+        // method main
         String fakultas;
+        // deklarasi variabel kosong dengan type data String
 }
 }
