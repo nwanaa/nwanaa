0d12f7143d91edccc53ca235a06d3b6eb2afc85c
nwanaa <winituuu@gmail.com>
2024-09-08 04:01:24 -0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,17 @@
+public class Day7 {
+    //bagian class
+    public static void main(String[] args) {
+        //method main
+        String nama;
+        //deklarasi variabel 
+        nama = "Wana";
+        //memberikan nilai awal pada variabel nama
+        
+        byte umur = 19;
+        // deklarasi variabel dengan memberikan nilai awal secara langsung
+        
+        System.out.println("Nama saya " + nama);
+        System.out.println("Saya berusia " + umur + " tahun");
+        // menampilkan output dari variabel yang telah dibuat
+}
+}
