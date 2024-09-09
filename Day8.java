1fcb50b8cd35656428bbb44e30475060fe982fad
nwanaa <winituuu@gmail.com>
2024-09-09 04:13:26 -0800

    update nilai variabel

---
+++ Day8.java
@@ -0,0 +1,18 @@
+public class Day8 {
+//bagian class
+    public static void main(String[] args) {
+    //method main
+    
+    String nama = "wana";
+    //mendeklarasikan variabel nama dengan tipe data String dan menginisialisasinya dengan nilai "wana".
+    
+        System.out.println("halo nama ku " + nama);
+        // outputnya ketika variabel nama belum di update
+    
+    nama = "wini";
+    // mengupdate nilai variabel nama dari wana menjadi wini
+    
+        System.out.println("halo nama ku " + nama);
+        // outputnya ketika variabel nama sudah di update
+}
+}
