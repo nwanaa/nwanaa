79b0bb906b517185aaf2abd66848ba8729cf6128
nwanaa <winituuu@gmail.com>
2024-09-10 01:44:22 -0800

    membuat biodata yang berisikan variabel final

---
+++ Day9.java
@@ -0,0 +1,15 @@
+public class Day9 {
+    // bagian class
+    public static void main(String[] args) {
+        // method main
+        
+        final String nama = "Nirwana";
+        final String nim = "D0223330";
+        final String kelas = "Informatika C";
+        final String prodi = "Informatika";
+        // mendeklarasikan variabel yang tidak dapat diubah (final) dan memberikan nilai awal kepada masing-masing variabel
+
+        System.out.print("Nama: " + nama + ", Nim: " + nim + ", Kelas: " + kelas + ", Prodi: " + prodi);
+        // menampilkan biodata dalam satu baris
+}
+}
