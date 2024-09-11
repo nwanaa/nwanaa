1f05e1d926e64bfda26a2ca5a89c1bae4449eed6
nwanaa <winituuu@gmail.com>
2024-09-11 20:23:50 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,12 @@
+public class Day10 {
+    // bagian class
+    public static void main(String[] args) {
+        // method main
+        
+        byte umur = 19;
+         //mendeklarasikan variabel umur dengan tipe data byte dan menginisialisasinya dengan nilai 19
+        
+         System.out.println("wana berumur " + umur + " tahun");
+         // menampilkan output dari variabel yang telah dibuat
+}
+}
