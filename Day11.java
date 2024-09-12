784a82599b15bc3db556afc34539e2540d8a21e8
nwanaa <winituuu@gmail.com>
2024-09-12 14:47:00 +0800

    type data short

---
+++ Day11.java
@@ -0,0 +1,11 @@
+public class Day11 {
+    // bagian class
+    public static void main(String[] args) {
+        // method main
+        short shoes = 37;
+        //mendeklarasikan variabel shoes dengan tipe data short dan menginisialisasinya dengan nilai 37
+        
+        System.out.println("ukuran sepatu wana: " + shoes);
+        // menampilkan output dari variabel yang telah dibuat
+}
+}
