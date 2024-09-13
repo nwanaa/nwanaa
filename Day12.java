057b87e670cabba89aed328f7f726f801e9aad6b
nwanaa <winituuu@gmail.com>
2024-09-13 21:28:01 +0800

    type data integer

---
+++ Day12.java
@@ -0,0 +1,13 @@
+public class Day12 {
+// bagian class
+    
+    public static void main(String[] args) {
+    // method main
+    
+    int date = 13;
+    //mendeklarasikan variabel date dengan tipe data integer dan menginisialisasinya dengan nilai 13
+    
+        System.out.println("day 12 adalah tanggal " + date + " september");
+         // menampilkan output dari variabel yang telah dibuat
+}
+}
