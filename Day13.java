0d4869f937dc64ff7a12dcebf6ce1f2387dd272f
nwanaa <winituuu@gmail.com>
2024-09-13 23:26:01 -0800

    evaluasi 1

---
+++ Day13.java
@@ -0,0 +1,29 @@
+public class Day13 {
+    public static void main(String[] args) {
+        // nomor 1
+        System.out.printf("nama: %s" , "wana");
+        System.out.println(" ");
+        System.out.printf("nim: %s" , "D0223330");
+        System.out.println(" ");
+        System.out.printf("umur: %d" , 19);
+        System.out.println(" ");
+        
+        // nomor 2
+        // deklarasi adalah membuat variabel menggunakan type data tertentu
+        /* inisialisasi yaitu memberikan nilai awal pada variabel */
+        
+        // nomor 3
+        final String nama = "wana";
+        
+        // nomor 4
+        int a = 1;
+        int b = 2;
+        int c = b;
+        
+        b = a;
+        a = c;
+        
+       System.out.println("a: " + a);       
+       System.out.println("b: " + b);
+}
+}
