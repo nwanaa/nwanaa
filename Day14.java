8360aa64ae627f283accea71b926125325b52282
nwanaa <winituuu@gmail.com>
2024-09-15 20:56:42 +0800

    tipe data long

--- Day14.java
+++ Day14.java
@@ -1,6 +1,6 @@
 public class Day14 {
     public static void main(String[] args) {
-        long norek = 2113467829L;
+        long norek = 21134678293L;
         System.out.println("nomor rekening wana: " + norek);
 }
 }
