d960ccd61acb90c7be709b6623673b073118e089
nwanaa <winituuu@gmail.com>
2024-09-16 07:13:46 -0800

    tipe data double

---
+++ Day15.java
@@ -0,0 +1,19 @@
+public class Day15 {
+    
+    public static void main(String[] args) {
+ 
+        double nilai = 7.2836482977;
+        
+        System.out.printf("%.1f%n", nilai); 
+        System.out.printf("%.2f%n", nilai); 
+        System.out.printf("%.3f%n", nilai); 
+        System.out.printf("%.4f%n", nilai); 
+        System.out.printf("%.5f%n", nilai); 
+        System.out.println(String.format("%.6f", nilai)); 
+        System.out.println(String.format("%.7f", nilai)); 
+        System.out.println(String.format("%.8f", nilai)); 
+        System.out.println(String.format("%.9f", nilai)); 
+        System.out.println(String.format("%.10f", nilai)); 
+    }
+}
+
