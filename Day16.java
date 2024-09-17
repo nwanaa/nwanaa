6e8e6fd58da2618f9593a02d528e5ead2de313ff
nwanaa <winituuu@gmail.com>
2024-09-17 05:26:28 -0800

    tipe data float

---
+++ Day16.java
@@ -0,0 +1,12 @@
+public class Day16 {
+// bagian class
+    public static void main(String[] args) {
+    // method main
+    
+    float bilanganpecahan = 30.8f;
+    //mendeklarasikan variabel bilanganpecahan dengan tipe data float dan menginisialisasinya dengan nilai 30.8
+    
+    System.out.println("contoh bilangan pecahan adalah " + bilanganpecahan);
+    // menampilkan output dari variabel yang telah dibuat
+}
+}
