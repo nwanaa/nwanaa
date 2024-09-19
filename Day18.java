8d30d60674edec2127a6e42343b742305ca0a9fa
nwanaa <winituuu@gmail.com>
2024-09-19 21:51:54 +0800

    tipe data boolean

--- Day18.java
+++ Day18.java
@@ -1,7 +1,8 @@
 public class Day18 {
     public static void main(String[] args) {
         boolean benar = true;
-        
+        boolean salah = false;
         System.out.println("benar: " + benar);
+        System.out.println("salah: " + salah);
 }
 }
