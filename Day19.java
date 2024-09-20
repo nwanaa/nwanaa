8ac5cc9b20ff6695fb89b70be915ec177d5c7e4a
nwanaa <winituuu@gmail.com>
2024-09-20 07:32:48 -0800

    type data reference : String

---
+++ Day19.java
@@ -0,0 +1,7 @@
+public class Day19 {
+    public static void main(String[] args) {
+        String mdpl = "alam tidak pernah gagal mengasihi luka";
+        System.out.println("3726 mdp : " + mdpl); 
+         
+}
+}
