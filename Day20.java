9156047c6dc0495a78eb537c524cf24f93f22807
nwanaa <winituuu@gmail.com>
2024-09-21 06:41:39 -0800

    tipe data reference byte

--- Day20.java
+++ Day20.java
@@ -1,6 +1,6 @@
 public class Day20 {
     public static void main(String[] args) {
-        byte day = 20;
+        Byte day = 20;
         System.out.println("hari ini adalah hari ke " + day);
 }
 }
