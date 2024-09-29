0f2dba4100b4e9d7fc958759c82024bf39bbccc3
nwanaa <winituuu@gmail.com>
2024-09-29 06:23:24 -0800

    tipe data reference boolean

---
+++ Day28.java
@@ -0,0 +1,10 @@
+public class Day28 {
+public static void main(String[] args) {
+   Boolean cewe = true;
+   Boolean cowo = false;
+   
+    System.out.println("cewe: " + cewe);
+    System.out.println("cowo: " + cowo);
+}
+    
+}
