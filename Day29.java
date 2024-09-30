6c6314d8265923631e338b50c44e385695416154
nwanaa <winituuu@gmail.com>
2024-09-30 05:17:40 -0800

    tipe data reference BigInteger

---
+++ Day29.java
@@ -0,0 +1,9 @@
+import java.math.BigInteger;
+
+public class Day29 {
+
+    public static void main(String[] args) {
+        BigInteger data = new BigInteger("99624273898658");
+        System.out.println("data: " + data);
+    }
+}
