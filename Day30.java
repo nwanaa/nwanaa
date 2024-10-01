c2e2a4524d9a2a5ba8f44a6eb8dd0fe5e3aa5696
nwanaa <winituuu@gmail.com>
2024-10-01 05:19:16 -0800

    tipe data reference BigDecimal

---
+++ Day30.java
@@ -0,0 +1,9 @@
+import java.math.BigDecimal;
+
+public class Day30 {
+public static void main(String[] args) {
+   BigDecimal nilai = new BigDecimal("302.56743");
+    System.out.println(nilai);
+}
+    
+}
