92596824cb9caac9e74cccba8797a90a8a351c27
nwanaa <winituuu@gmail.com>
2024-10-03 06:15:47 -0800

    konversi dari primitif ke string

---
+++ Day32.java
@@ -0,0 +1,37 @@
+public class Day32 {
+public static void main(String[] args) {
+    byte w1 = 8;
+    String b1 = String.valueOf(w1);
+    System.out.println("byte: " + b1);
+    
+    short w2 = 16;
+    String b2 = String.valueOf(b1);
+    System.out.println("short: " + b2);
+    
+    int w3 = 30;
+    String b3 = String.valueOf(b2);
+    System.out.println("int: " + b3);
+    
+    long w4 = 30825748;
+    String b4 = String.valueOf(b3);
+    System.out.println("long: " + b4);
+    
+    float w5 = 30.8f;
+    String b5 = String.valueOf(b4);
+    System.out.println("float: " + b5);
+    
+    double w6 = 8.16;
+    String b6 = String.valueOf(b5);
+    System.out.println("double: " + b6);
+    
+    char w7 = 'w';
+    String b7 = String.valueOf(b6);
+    System.out.println("char: " + b7);
+    
+    boolean w8 = true;
+    String b8 = String.valueOf(b7);
+    System.out.println("boolean: " + b8);
+    
+}
+    
+}
