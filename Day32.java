e4a427fff01cf7fd37f0d854da83af07a95cba00
nwanaa <winituuu@gmail.com>
2024-10-03 06:39:19 -0800

    konversi tipe data primitif ke string

--- Day32.java
+++ Day32.java
@@ -5,31 +5,31 @@ public static void main(String[] args) {
     System.out.println("byte: " + b1);
     
     short w2 = 16;
-    String b2 = String.valueOf(b1);
+    String b2 = String.valueOf(w2);
     System.out.println("short: " + b2);
     
     int w3 = 30;
-    String b3 = String.valueOf(b2);
+    String b3 = String.valueOf(w3);
     System.out.println("int: " + b3);
     
-    long w4 = 30825748L;
-    String b4 = String.valueOf(b3);
+    long w4 = 30825748;
+    String b4 = String.valueOf(w4);
     System.out.println("long: " + b4);
     
     float w5 = 30.8f;
-    String b5 = String.valueOf(b4);
+    String b5 = String.valueOf(w5);
     System.out.println("float: " + b5);
     
     double w6 = 8.16;
-    String b6 = String.valueOf(b5);
+    String b6 = String.valueOf(w6);
     System.out.println("double: " + b6);
     
     char w7 = 'w';
-    String b7 = String.valueOf(b6);
+    String b7 = String.valueOf(w7);
     System.out.println("char: " + b7);
     
     boolean w8 = true;
-    String b8 = String.valueOf(b7);
+    String b8 = String.valueOf(w8);
     System.out.println("boolean: " + b8);
     
 }
