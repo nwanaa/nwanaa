129551d33bf48d07aee64131ff5f4d15f0725a20
nwanaa <winituuu@gmail.com>
2024-10-04 06:23:02 -0800

    konversi tipe data primitif ke tipe data primitif

---
+++ Day33.java
@@ -0,0 +1,31 @@
+
+public class Day33 {
+
+    public static void main(String[] args) {
+        byte w1 = 30;
+        int b1 = (int) w1;
+        System.out.println("byte: " + b1);
+
+        short w2 = 16;
+        int b2 = (int) w2;
+        System.out.println("short: " + b2);
+
+        int w3 = 50;
+        double b3 = (double) w3;
+        System.out.println("int: " + b3);
+
+        long w4 = 6074836L;
+        int b4 = (int) w4;
+        System.out.println("long: " + b4);
+
+        float w5 = 8.30f;
+        int b5 = (int) w5;
+        System.out.println("float: " + b5);
+
+        double w6 = 8.3;
+        int b6 = (int) w6;
+        System.out.println("double: " + b6);
+
+    }
+
+}
