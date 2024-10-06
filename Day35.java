567d1a9c8d17f5af43fcd9853c467b832fc0227e
nwanaa <winituuu@gmail.com>
2024-10-06 07:37:01 -0800

    operator aritmatika : penjumlahan dan pengurangan

---
+++ Day35.java
@@ -0,0 +1,15 @@
+
+public class Day35 {
+
+    public static void main(String[] args) {
+        int angka1 = 30;
+        int angka2 = 8;
+
+        int penjumlahan = angka1 + angka2;
+        int pengurangan = angka1 - angka2;
+
+        System.out.println("hasil penjumlahan: " + penjumlahan);
+        System.out.println("hasil pengurangan: " + pengurangan);
+    }
+
+}
