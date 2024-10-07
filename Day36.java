040b7524e8fe1c8fb3a9ffadfcf29b3ce8110243
nwanaa <winituuu@gmail.com>
2024-10-07 04:41:54 -0800

    perkalian, pembagian, modulo

---
+++ Day36.java
@@ -0,0 +1,14 @@
+public class Day36 {
+public static void main(String[] args) {
+   int w1 = 30;
+   int w2 = 2;
+   
+   int kali = w1 * w2;
+   int bagi = w1 / w2;
+   int modulus = w1 % w2;
+   
+    System.out.println("hasil kali: " + kali);
+    System.out.println("hasil bagi: " + bagi);
+    System.out.println("sisa bagi: " + modulus);
+}
+}
