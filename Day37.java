c21e45020aa541058d23cae3df4b52ed8c53cd7c
nwanaa <winituuu@gmail.com>
2024-10-08 06:14:59 -0800

    operator penugasan yaitu pengisian nilai, pengisian dan penambahan, pengisian dan pengurangan

---
+++ Day37.java
@@ -0,0 +1,15 @@
+public class Day37 {
+
+    public static void main(String[] args) {
+        int mdpl = 30;
+        System.out.println("nilai: " + mdpl);
+
+        mdpl += 8;
+        System.out.println("hasil variabel nilai setelah ditambah 8: " + mdpl);
+
+        mdpl -= 16;
+        System.out.println("hasil variabel nilai setelah dikurangi 16: " + mdpl);
+
+    }
+
+}
