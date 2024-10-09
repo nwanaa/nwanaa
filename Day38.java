e901e7b0f3a8c04ab38a5e2da561633f21230de6
nwanaa <winituuu@gmail.com>
2024-10-09 22:54:43 +0800

    Operator Penugasan : Pengisian dan Perkalian, Pengisian dan  Pembagian

---
+++ Day38.java
@@ -0,0 +1,13 @@
+public class Day38 {
+public static void main(String[] args) {
+    int neto = 20;
+    System.out.println("neto: " + neto);
+    
+    neto *= 4;
+    System.out.println("hasil variabel neto setelah dikali 4: " + neto);
+    
+    neto /= 5;
+    System.out.println("hasil variabel neto setelah dibagi 5: " + neto);
+}
+    
+}
