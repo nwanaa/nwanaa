53bf8b4b2f4cf1b056feb582c4b073486cc767b6
nwanaa <winituuu@gmail.com>
2024-10-11 07:13:41 -0800

    operator perbandingan: lebih besar dan lebih kecil

---
+++ Day40.java
@@ -0,0 +1,15 @@
+public class Day40 {
+
+    public static void main(String[] args) {
+        byte num1 = 30;
+        int num2 = 8;
+
+        boolean sar = num1 > num2;
+        boolean cil = num1 < num2;
+
+        System.out.println("lebih besar i num1 daripada num2: " + sar);
+        System.out.println("lebih kecil i num1 daripada num2: " + cil);
+
+    }
+
+}
