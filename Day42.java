8ef9791ac6766aac8986cdcb6e6ad6ab01beae7c
nwanaa <winituuu@gmail.com>
2024-10-13 23:12:28 +0800

    operator perbandingan: sama dengan dan tidak sama dengan

---
+++ Day42.java
@@ -0,0 +1,15 @@
+public class Day42 { 
+
+    public static void main(String[] args) {
+        int w1 = 30;
+        int w2 = 8; 
+
+        boolean sama = w1 == w2;
+        boolean tidak = w1 != w2; 
+
+        System.out.println("w1 sama dengan w2: " + sama);
+        System.out.println("w1 tidak sama dengan w2: " + tidak); 
+
+    } 
+
+}
