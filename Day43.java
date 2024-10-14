377a70a32618c792d18bf0031d1708e3a1d9494c
nwanaa <winituuu@gmail.com>
2024-10-14 07:22:31 -0800

    operator perbandingan: lebih besar sama dengan dan lebih kecil sama dengan

---
+++ Day43.java
@@ -0,0 +1,15 @@
+public class Day43 {
+
+    public static void main(String[] args) {
+        byte angka1 = 1;
+        byte angka2 = 9;
+
+        boolean sarsem = angka1 >= angka2;
+        System.out.println("angka1 lebih besar dari angka2: " + sarsem);
+
+        boolean cilsem = angka1 <= angka2;
+        System.out.println("angka1 lebih kecil dari angka2: " + cilsem);
+
+    }
+
+}
