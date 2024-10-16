dfde1524917cf2b9932c6b3e9aaa01955d77a9c6
nwanaa <winituuu@gmail.com>
2024-10-16 06:13:09 -0800

    operator logika or

---
+++ Day45.java
@@ -0,0 +1,10 @@
+public class Day45 {
+public static void main(String[] args) {
+    byte md = 37;
+    byte pl = 26;
+    
+    System.out.println(md == pl || md < pl);
+    System.out.println(md == pl || md > pl);
+}
+    
+}
