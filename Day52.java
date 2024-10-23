a94aca5b35ef5c0a0c7157bf522c3107c6185718
nwanaa <winituuu@gmail.com>
2024-10-23 06:24:09 -0800

    operator ternary

---
+++ Day52.java
@@ -0,0 +1,10 @@
+public class Day52 {
+
+    public static void main(String[] args) {
+        int nilai = 95;
+        String status = (nilai >= 70)? "lulus" : "tidak lulus";
+
+        System.out.println("status: " + status);
+    }
+
+}
