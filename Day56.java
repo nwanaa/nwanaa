60a8b144da4cecb1ade6139a1efb43d63722bbc6
nwanaa <winituuu@gmail.com>
2024-10-28 06:06:23 -0800

    looping while increment

---
+++ Day56.java
@@ -0,0 +1,16 @@
+public class Day56 {
+
+    public static void main(String[] args) {
+        byte w = 0;
+
+        while (w <= 20) {
+            w++;
+            if (w % 2 == 0) {
+                
+                System.out.print(w + " ");
+
+            }
+        }
+    }
+
+}
