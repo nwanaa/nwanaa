a83dabb70f08a8ae1bf81bacbbd0d5bda3486eb5
nwanaa <winituuu@gmail.com>
2024-10-29 01:44:19 -0800

    looping while decrement

---
+++ Day57.java
@@ -0,0 +1,15 @@
+public class Day57 {
+
+    public static void main(String[] args) {
+        short r = 30;
+
+        while (r > 1) {
+            r--;
+            if (r % 2 != 0 && r % 3 == 0) {
+
+                System.out.print(r + " ");
+            }
+        }
+    }
+
+}
