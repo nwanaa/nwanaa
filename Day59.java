f75062c37b169771c9c483baf714d0f8a4e678a0
nwanaa <winituuu@gmail.com>
2024-10-30 04:30:30 -0800

    looping for each

---
+++ Day59.java
@@ -0,0 +1,10 @@
+public class Day59 {
+public static void main(String[] args) {
+   String nama[] = {"wana", "wini", "wonu"};
+        
+        for( String w : nama ){
+            System.out.print(w + " ");
+        }
+
+    }
+}
