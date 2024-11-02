4e08994e89c5faead1c59fc66dd4c5a3b6b3695b
nwanaa <winituuu@gmail.com>
2024-11-02 04:00:30 -0800

    Buatlah program java yang meminta pengguna memasukkan angka berurutan. Jika pengguna memasukkan angka berurut program tidak akan berhenti meminta inputan, tetapi jika pengguna memasukkan angka selain urutannya maka program akan berhenti dan menampilkan pesan “angka tidak berurut”.

---
+++ Day62.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Day62 {
+
+    public static void main(String[] args) {
+        Scanner wana = new Scanner(System.in);
+
+        int angka_awal = wana.nextInt();
+        boolean loop = true;
+        
+        while (loop) {
+            int angka = wana.nextInt();
+            if (angka == angka_awal + 1) {
+                angka_awal = angka;
+            } else {
+                System.out.println("angka tidak berurut");
+                loop = false;
+            }
+        }
+    }
+
+}
