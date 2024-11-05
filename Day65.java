d28e2211d814f8fc9264baaad4ebafd87c5c3460
nwanaa <winituuu@gmail.com>
2024-11-05 07:44:48 -0800

    nested loop

---
+++ Day65.java
@@ -0,0 +1,14 @@
+public class Day65 {
+
+    public static void main(String[] args) {
+        int minggu = 2;
+
+        for (int i = 1; i <= minggu; ++i) {
+            System.out.println("minggu ke: " + i);
+
+            for (int j = 1; j <= 7; ++j) {
+                System.out.println("hari: " + j);
+            }
+        }
+    }
+}
