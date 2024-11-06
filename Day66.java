1b732626ed8472689eb6eac0bf4641969a4b0f55
nwanaa <winituuu@gmail.com>
2024-11-06 06:32:36 -0800

    pola persegi dan persegi panjang

--- Day66.java
+++ Day66.java
@@ -5,23 +5,12 @@ public class Day66 {
     public static void main(String[] args) {
         Scanner wana = new Scanner(System.in);
 
-        System.out.print("masukkan sisi persegi: ");
-        int sisi = wana.nextInt();
-
-        System.out.println("pola persegi:");
-        for (int i = 0; i < sisi; i++) {
-            for (int j = 0; j < sisi; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-
-        System.out.print("\nmasukkan panjang untuk persegi panjang: ");
+        System.out.print("panjang: ");
         int panjang = wana.nextInt();
-        System.out.print("masukkan lebar untuk persegi panjang: ");
-        int lebar = wana.nextInt();
 
-        System.out.println("pola persegi panjang:");
+        System.out.print("lebar: ");
+        int lebar = wana.nextInt();
+        
         for (int i = 0; i < lebar; i++) {
             for (int j = 0; j < panjang; j++) {
                 System.out.print("* ");
