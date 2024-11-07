d50b5d0ffe168bfa10623f1d06cd5fe12c9a3bb3
nwanaa <winituuu@gmail.com>
2024-11-07 04:34:00 -0800

    method void

---
+++ Day67.java
@@ -0,0 +1,12 @@
+public class Day67 {
+
+    public void wana() {
+        System.out.println("3726 mdpl");
+    }
+
+    public static void main(String[] args) {
+        Day67 mdpl = new Day67();
+        mdpl.wana();
+    }
+
+}
