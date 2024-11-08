9c5a6a3bc5d1749a9692ce9dfbffd10bd66c0cfb
nwanaa <winituuu@gmail.com>
2024-11-08 06:00:53 -0800

    method non void

---
+++ Day68.java
@@ -0,0 +1,9 @@
+public class Day68 {
+    public static void main(String[] args) {
+        Day68 wana = new Day68();
+        System.out.println(wana.mdpl());
+    }
+        int mdpl() {
+        return 3726;
+    }
+}
