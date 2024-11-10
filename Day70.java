2111e5283a4979a33133fef131538700561c00ed
nwanaa <winituuu@gmail.com>
2024-11-10 05:43:13 -0800

    method parameter

---
+++ Day70.java
@@ -0,0 +1,14 @@
+public class Day70 {
+
+    public static void main(String[] args) {
+        int panjang = 30;
+        int lebar = 8;
+
+        hitungluas(panjang, lebar);
+    }
+
+    static void hitungluas(int panjang, int lebar) {
+        int luas = panjang * lebar;
+        System.out.println(luas);
+    }
+}
