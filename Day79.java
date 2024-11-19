7f5268a2deaa18776f41d40879cd6ea788e7b27e
nwanaa <winituuu@gmail.com>
2024-11-19 04:38:54 -0800

    String method: toString()

---
+++ Day79.java
@@ -0,0 +1,19 @@
+public class Day79 {
+
+    public static void main(String[] args) {
+        Integer a = 456;
+        System.out.println("Integer : " + a.toString());
+
+        Double b = 78.9;
+        System.out.println("Double  : " + b.toString());
+
+        Float c = 34.56f;
+        System.out.println("Float   : " + c.toString());
+
+        Long d = 987654321L;
+        System.out.println("Long    : " + d.toString());
+
+        Boolean e = false;
+        System.out.println("Boolean : " + e.toString());
+    }
+}
