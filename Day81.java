a13fea63efcf013ca0e8df84820f4f1a620642d7
nwanaa <winituuu@gmail.com>
2024-11-21 06:37:10 -0800

    String method: valueOf()

---
+++ Day81.java
@@ -0,0 +1,11 @@
+public class Day81 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 30; i++) {
+
+            String w = String.valueOf(i);
+            System.out.println("angka sebagai string: " + w);
+        }
+    }
+}
